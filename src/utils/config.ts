67aae568ac7abc08cd4071fc2455f9dcdee1542e
danju4rizzl <danjumashiwaju@gmail.com>
2024-08-15 00:15:37 +0200

    major update for LaunchPad

---
+++ src/utils/config.ts
@@ -0,0 +1,23 @@
+export const pricingList = {
+  LunchPad: {
+    packageName: 'LunchPad',
+    packageLink:
+      'https://deejaydev.lemonsqueezy.com/buy/53b26d0e-316a-4a54-9b91-32e7ad42be71',
+    priceInUsd: 16.99,
+    price: 299
+  },
+  GrowthHub: {
+    packageName: 'GrowthHub',
+    packageLink:
+      'https://deejaydev.lemonsqueezy.com/buy/8b803a1e-25ce-4d77-89d2-c764ea9c8075',
+    priceInUsd: 24.99,
+    price: 499
+  },
+  SuccessEngine: {
+    packageName: 'SuccessEngine',
+    packageLink:
+      'https://deejaydev.lemonsqueezy.com/buy/c872fcda-793f-43a5-9c3b-58876fad8a36',
+    priceInUsd: 49.99,
+    price: 799
+  }
+}
